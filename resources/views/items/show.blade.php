9954a61e6ca0a8367f62d37b6bab0c274cd8dffc
dmiyakoshi <d1m354dai@gmail.com>
2021-09-18 14:04:24 +0900

    削除機能実装

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>show</title>
-    </head>
-    <body>
-        <p>
-            <b>商品名:{{ $item->name }}</b>
-        </p>
-        <p>
-            <b>商品詳細:{{ $item->description }}</b>
-        </p>
-        <p>
-            <b>価格:{{ $item->price }}</b>
-        </p>
-        <p>
-            <b>出品者:{{ $item->seller }}</b>
-        </p>
-        <p>
-            <b>電子メール:{{ $item->email }}</b>
-        </p>
-        <p>
-            <img src="{{ $item->image_url }}" alt="">
-        </p>
 
-        <a href="/items/{{ $item->id }}/edit">編集する</a>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>show</title>
+</head>
+
+<body>
+    <p>
+        <b>商品名:{{ $item->name }}</b>
+    </p>
+    <p>
+        <b>商品詳細:{{ $item->description }}</b>
+    </p>
+    <p>
+        <b>価格:{{ $item->price }}</b>
+    </p>
+    <p>
+        <b>出品者:{{ $item->seller }}</b>
+    </p>
+    <p>
+        <b>電子メール:{{ $item->email }}</b>
+    </p>
+    <p>
+        <img src="{{ $item->image_url }}" alt="">
+    </p>
+
+    <!--<a href="/items/{{ $item->id }}/edit">編集する</a> -->
+    <button type="button" onclick="location.href='/items/{{ $item->id }}/edit'">編集する</button>
+    <input type="submit" value="削除する" id="delete-form" onclick="if(!confirm('本当に削除しますか？')){return false}"> 
+    <form action="/items/{{ $item->id }}" method="POST" id="delete-form">
+        @csrf
+        @method('DELETE')
+    </form>
+</body>
